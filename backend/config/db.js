d9fc383aebebe9670893234faf908357659ab553
Yash-11111 <yashkesarwani8040@gmail.com>
2026-04-18 16:23:07 +0530

    db update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/hospitalDB");
+    const conn = await mongoose.connect("mongodb+srv://yash:yash1234@cluster0.e9h1vd6.mongodb.net/?appName=Cluster0");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
